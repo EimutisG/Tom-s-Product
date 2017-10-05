1f1980b174418ed5b61952715a4991b4e37278b8
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2017-10-05 22:24:41 +0100

    ale

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
+import { StarRatingComponent } from './shared/star-rating/star-rating.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpaces,
+    StarRatingComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, 
+    HttpModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../product';
+import { ProductService } from '../shared/product.service';
+
+@Component({
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+    pageTitle: string = "John`s product";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = true;
+
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    filteredProducts: IProduct[];
+    products: IProduct[] ;
+
+     constructor( private _productService  : ProductService){
+         this.filteredProducts= this.products;
+     }
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) != -1);
+    }
+
+    toggleImage(): void {
+
+        this.showImage = !this.showImage;
+    }
+    ngOnInit(): void {
+        this.products= this._productService.getProducts();
+        this.filteredProducts= this.products;
+    }
+
+}
